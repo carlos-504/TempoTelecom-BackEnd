ddb856a4f7b251ddfb89b5ef0868172e55b167c4
carlos-504 <carllosshennriique@gmail.com>
2021-06-22 13:42:01 -0300

    Initial commit

---
+++ src/migrations/20210622153013-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      number: {
+        type: Sequelize.STRING,
+      },
+      birthDate: {
+        type: Sequelize.DATEONLY,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
+};
